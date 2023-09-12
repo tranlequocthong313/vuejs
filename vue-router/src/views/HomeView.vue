f68d4c87126ca51e7fb7208975ede6f4bd2cd131
tranlequocthong313 <tranlequocthong313@gmail.com>
2023-09-12 23:34:57 +0700

    finished vue router

---
+++ vue-router/src/views/HomeView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+    <h1>Home page</h1>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi eius minus,
+      quasi, quod commodi ipsa deleniti aspernatur tempora excepturi odio
+      suscipit consectetur repellat impedit atque aliquid numquam consequuntur
+      cum vitae!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi eius minus,
+      quasi, quod commodi ipsa deleniti aspernatur tempora excepturi odio
+      suscipit consectetur repellat impedit atque aliquid numquam consequuntur
+      cum vitae!
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeView',
+  components: {},
+};
+</script>
---
+++ vue-router/src/views/jobs/JobView.vue
@@ -0,0 +1,44 @@
+<template>
+  <h1>Jobs page</h1>
+  <ol type="1">
+    <li v-for="job in jobs" :key="job.id">
+      <router-link :to="{ name: 'jobDetails', params: { id: job.id } }">
+        {{ job.name }} - {{ job.description }}
+      </router-link>
+    </li>
+  </ol>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      jobs: [
+        { id: 1, name: 'IT', description: 'lorem' },
+        { id: 2, name: 'QA', description: 'lorem' },
+        { id: 3, name: 'HR', description: 'lorem' },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+li {
+  font-size: 20px;
+  font-weight: bold;
+  padding: 20px;
+  border-radius: 12px;
+  background-color: #f4f4f4;
+  margin: 10px;
+  max-width: 600px;
+  margin: 10px auto;
+}
+li:hover {
+  background-color: #ddd;
+}
+li a {
+  text-decoration: none;
+  color: #444;
+}
+</style>
