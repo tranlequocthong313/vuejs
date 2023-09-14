92189bfdea3395025ecda80e01e4073482d34c2d
tranlequocthong313 <tranlequocthong313@gmail.com>
2023-09-15 00:03:59 +0700

    finished vuetify

---
+++ vuetify/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { ref } from 'vue';
+import ImageGallary from './components/ImageGallary.vue';
+import Sidebar from './components/Sidebar.vue';
+
+const isWithColor = ref(true);
+</script>
+
+<template>
+  <VApp>
+    <!-- SIDEBAR -->
+    <Sidebar @select-option="(isColor) => (isWithColor = isColor)" />
+    <VMain>
+      <!-- CARDS -->
+      <ImageGallary :isWithColor="isWithColor" />
+    </VMain>
+  </VApp>
+</template>
---
+++ vuetify/src/components/ImageGallary.vue
@@ -0,0 +1,38 @@
+<script setup>
+import Image from './Image.vue';
+const props = defineProps({
+  isWithColor: {
+    type: Boolean,
+  },
+});
+
+const copyURL = async (url) => {
+  await navigator.clipboard.writeText(url);
+};
+</script>
+
+<template>
+  <VCard class="mx-8 my-2 pa-3">
+    <VRow>
+      <VCol sm="6" md="2" lg="1" v-for="n in 200">
+        <Image
+          @on-click="
+            copyURL(
+              `https://picsum.photos/500/300?image=${n * 5 + 10}&${
+                isWithColor ? '' : 'grayscale'
+              }`
+            )
+          "
+          :src="`https://picsum.photos/500/300?image=${n * 5 + 10}&${
+            isWithColor ? '' : 'grayscale'
+          }`"
+          :lazy-src="`https://picsum.photos/10/6?image=${n * 5 + 10}&${
+            isWithColor ? '' : 'grayscale'
+          }`"
+          :aspect-ratio="1"
+          :cover="true"
+        />
+      </VCol>
+    </VRow>
+  </VCard>
+</template>
